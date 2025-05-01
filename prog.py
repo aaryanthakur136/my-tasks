e898a787ddc0657eff1917cf8a756618943271ed
aaryanthakur136 <aaryansingh362@gmail.com>
2025-05-01 14:04:47 +0530

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,19 @@
+print("hello world")
+print(10+20)
+print(2**3)
+print(5/2)
+print(5//2)
+print(58%5)
+print(569%9)
+print(10%3)
+print(782.434%43)
+print('python\'s "world" ')
+print("i am a polite person \n" * 3)
+print(' item \t rate\n 5 \t       4\n 4 \t       5 ' )
+print(55
+
+)
+a="hello"
+b=" world"
+c=a+b
+print(c)
